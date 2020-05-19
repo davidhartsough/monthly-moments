1f80f34b1c70480947454aeca8865b912c35d0b0
David Hartsough <hartsoughdavid@gmail.com>
2020-05-19 13:25:58 -0600

    blacklist profile and requests from redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -7,7 +7,7 @@ import rootReducer from "./reducers";
 const persistConfig = {
   key: "root_monthly_moments",
   storage,
-  blacklist: ["auth"],
+  blacklist: ["auth", "profile", "requests"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
