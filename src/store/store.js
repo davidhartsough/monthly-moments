747600cd0e5602e7dcbee001f114005d2af2b277
David Hartsough <hartsoughdavid@gmail.com>
2020-05-19 04:43:42 -0600

    switch to session storage for redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage/session";
 import rootReducer from "./reducers";
 
 const persistConfig = {
   key: "root_monthly_moments",
   storage,
-  whitelist: ["auth", "thisMonthsMoments"],
+  blacklist: ["auth"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
