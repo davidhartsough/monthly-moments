44702c3d3d51ddcde0ebd2e045a0c15951eced04
David Hartsough <hartsoughdavid@gmail.com>
2020-07-20 10:40:55 -0600

    refine use of service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./normalize.css";
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
+import configureServiceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// serviceWorker.unregister();
+configureServiceWorker();
